2ab8364b538e46f61b27d4036237d9864a6be76a
Dubbie <dubbyezeabasili56@gmail.com>
2024-12-14 17:35:04 +0000

    Add custom alert functionality and styling; implement input validation

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-const pop_up = document.querySelector('.pop-up');   
+const pop_up = document.querySelector('.pop-up');
+const customAlert = document.querySelector('.alert');
+let alert_theme = document.querySelector("html").getAttribute("alert-theme");
+let alertType = document.querySelector(".alertType");
+const alertMessage = document.querySelector(".message");   
+
+// using function constructor
+function toggleAlert(message, type, theme){
+    alertMessage.textContent = message
+    alertType.textContent = type
+    alert_theme = theme
+    customAlert.classList.add("animate")
+    setTimeout(()=>{
+        customAlert.classList.remove("animate")
+    }, 5000)
+}
+
 
 function random () {
     //generate 4 random numbers
@@ -32,6 +48,7 @@ const submit = document.querySelector(".submit")
 submit.addEventListener('click', function(){
     if(this.textContent == "Get Code"){
         this.textContent = "Submit Code"
+        inputs[0].focus()
         let code = random().join("-")
         pop_up.querySelector('.code2').textContent = code;
         pop_up.classList.add("animate")
@@ -44,13 +61,20 @@ submit.addEventListener('click', function(){
             document.querySelector('.exp_note').textContent = `Code expires in ${timer} secs`
 
             if(timer <= 0){
-                pop_up.classlist.remove('animate')
+                clearInterval(interval) // Corrected
+                pop_up.classList.remove('animate') // Corrected
                 pop_up.querySelector('.code2').textContent = ''
                 document.querySelector('.code1').textContent = ''
                 document.querySelector('.exp_note').textContent = 'Code Expired'
                 this.textContent = "Get Code"
-                clearInterval(interval)
             }
-    }, 1000)
-}
-})
+        }, 1000)
+    } else {
+        for (let i = 0; i < inputs.length; i++){
+            if(inputs[i].value == ''){
+                toggleAlert("Please fill in all fields", "Error", "error")  
+                break;
+            }
+        }
+    }
+});
